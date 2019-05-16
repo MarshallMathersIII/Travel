b7c5595c9118122ec73f303162e9cfcbe4febbc4
eminem <ghy1060627080@163.com>
2019-05-16 21:44:59 +0800

    轮播图

--- src/main.js
+++ src/main.js
@@ -4,13 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+// require styles
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 // 使用 astClick移动端300毫秒点击延时问题
 fastClick.attach(document.body)
+// 轮播插件
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="wrapper">
+<swiper :options="swiperOption" >
+    <!-- slides -->
+    <swiper-slide v-for= "item of swiperList" :key="item.id">
+        <img class="swiper-img"  :src="item.imgUrl">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+</swiper>
+</div>
+</template>
+
+<script>export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/17e2352b857f66e9d2948e98dc483bc2.jpg_750x200_2857d0a3.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/d7bbc21db442366a882e04ddc984669a.jpg_750x200_85e640d9.jpg'
+      }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+.wrapper
+    overflow : hidden
+    width : 100%
+    height : 0
+    padding-bottom 26.00%
+    background : #eee
+    .swiper-img
+        width : 100%
+</style>
