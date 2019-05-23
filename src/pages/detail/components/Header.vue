ee9a2d249d4ce6a99dd2e20c284af649575aa2cd
eminem <ghy1060627080@163.com>
2019-05-23 18:00:39 +0800

    fix全局事件绑定移除

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -39,7 +39,11 @@ export default {
   },
   activated () {
     window.addEventListener('scroll', this.handleScroll)
+  },
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
   }
+
 }
 </script>
 <style lang="stylus" scoped>
