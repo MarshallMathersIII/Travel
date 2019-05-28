6f7ee9df3f29ee6f3cfddf325fdc67481efd0126
eminem <ghy1060627080@163.com>
2019-05-28 10:40:41 +0800

    详情页递归组件调用

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,45 @@
 <div>
 <detail-banner></detail-banner>
 <detail-header></detail-header>
-<div class="content"></div>
+<div class="content">
+  <detail-list :list="list"></detail-list>
+</div>
 </div>
 </template>
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人票绅士手',
+          children: [{
+            title: '小孩子'
+          }, {
+            title: '小孩子'
+          }]
+        }, {
+          title: '成人票五官'
+        }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '学2票'
+      }, {
+        title: '学23票'
+      }]
+    }
   }
 }
 </script>
